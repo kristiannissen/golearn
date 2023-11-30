e8af0a95c3183c8fc59b903927c2d1f10abccaab
kristiannissen <kristian.nissen@gmail.com>
2023-11-30 21:01:35 +0100

    Updating hellokitty

--- hellokitty/main.go
+++ hellokitty/main.go
@@ -1,7 +1,16 @@
+// Package HelloKitty explores the fmt package from the std
+// look at https://pkg.go.dev/fmt@go1.21.4
 package main
 
 import "fmt"
 
 func main() {
+	// Print a line of text
+	// Prints Hello Kitty
 	fmt.Println("Hello Kitty!")
+	// Print type using %T
+	var name string
+	name = "Kitty"
+	// Prints Name is string
+	fmt.Printf("Name is %T\n", name)
 }
