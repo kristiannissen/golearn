49171e186386e27702723970524e03a72ac85826
kristiannissen <kristian.nissen@gmail.com>
2023-12-10 03:36:07 +0100

    Adding

--- command-line/main.go
+++ command-line/main.go
@@ -17,11 +17,20 @@ func main() {
 	var name string
 	flag.StringVar(&name, "name", "Pussy", "The name")
 
+	num := flag.Int("number", 1, "")
+
 	flag.Parse()
 
 	// go run main.go -name=Kitty Prints: Name Kitty
 	// go run main.go Prints: Name "Pussy"
 	fmt.Println("Name", name)
+	// go run main.go -number=22 Prints: Number 23
+	fmt.Println("Number", addOne(*num))
 	// Vis alle
 	fmt.Println(flag.Args())
+}
+
+// Adds 1 to the number passed
+func addOne(n int) int {
+	return n + 1
 }
