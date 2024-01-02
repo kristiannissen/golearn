0caf0632034dc96ca48ad573c0aefd9bce7de1df
kristiannissen <kristian.nissen@gmail.com>
2024-01-02 18:27:52 +0100

    embedding

--- structs-methods/main.go
+++ structs-methods/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 type animal struct {
 	sound string
@@ -12,6 +16,13 @@ func (a *animal) eats(s string) {
 	a.food = s
 }
 
+// Embedding
+type RingerStringer struct {
+	Inp string
+	// RingerStringer kan nu bruge RingerStringer.Println
+	*log.Logger
+}
+
 func main() {
 	//
 	a := animal{sound: "Puurrr", class: "Mammals"}
@@ -20,4 +31,9 @@ func main() {
 	//
 	a.eats("Mice and shit")
 	fmt.Println("Eats", a.food)
+
+	// RingerStringer
+	r := &RingerStringer{"Hello", log.New(os.Stderr, "Ringer: ", log.Ldate)}
+	r.Printf("Hello Ringer Stringer %s", r.Inp)
+	// Output: Ringer: 2024/01/02 Hello Ringer Stringer Hello
 }
